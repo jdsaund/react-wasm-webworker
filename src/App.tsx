92a97ddcae08aecb068d633cfedf598217560520
Jolyon Saunders <jol.saund@gmail.com>
2022-09-04 15:33:45 +1000

    Connected wasm worker to react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import init, { greet } from 'wasm-worker'
+
 function App() {
+  useEffect(() => {
+    init().then(() => {
+      greet()
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
